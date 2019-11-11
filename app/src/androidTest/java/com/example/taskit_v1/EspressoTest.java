6d15b924ab1a4876b93288a11518a1bca2580558
Shiv <shivbirr@gmail.com>
2019-11-10 22:24:39 -0800

    First espresso test to help configure jenkins

--- app/src/androidTest/java/com/example/taskit_v1/EspressoTest.java
+++ app/src/androidTest/java/com/example/taskit_v1/EspressoTest.java
@@ -7,9 +7,12 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class EspressoTest {
 
@@ -17,7 +20,9 @@ public class EspressoTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
     @Test
     public void ensureTextChangesWork() {
-        
+        onView(withId(R.id.editText)).perform(typeText("espressotest"),closeSoftKeyboard());
+        onView(withId(R.id.save_btn)).perform(click());
+        onView(withId(R.id.displayText)).check(matches(withText("espressotest")));
 
     }
 
