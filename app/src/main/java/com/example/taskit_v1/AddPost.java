57093f0508adc10d3557386655e43650e5c6074e
Shiv <shivbirr@gmail.com>
2019-11-12 02:24:29 -0800

    commit _d1

---
+++ app/src/main/java/com/example/taskit_v1/AddPost.java
@@ -0,0 +1,14 @@
+package com.example.taskit_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
